34170193237c4dd4fb47f6d6721d44f922911c7a
Sandy Dai <sandydai98@gmail.com>
2021-03-01 16:48:10 -0500

    feat(models): adds address, candle, & user models

---
+++ server/db/models/Candle.js
@@ -0,0 +1,45 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Candle = db.define("candle", {
+	name: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			notEmpty: true
+		}
+	},
+	description: {
+		type: Sequelize.TEXT,
+		allowNull: false,
+		validate: {
+			notEmpty: true
+		}
+	},
+	imageUrl: {
+		type: Sequelize.TEXT,
+		defaultValue: "../../public/image.png",
+		validate: {
+			notEmpty: true
+		}
+	},
+	quantity: {
+		type: Sequelize.INTEGER,
+		allowNull: false,
+		validate: {
+			notEmpty: true
+		}
+	},
+	price: {
+		type: Sequelize.DECIMAL(10, 2),
+		allowNull: false,
+		validate: {
+			notEmpty: true
+		}
+	},
+	size: {
+		type: Sequelize.ENUM(["2 oz", "3 oz", "9 oz"])
+	}
+});
+
+module.exports = Candle;
