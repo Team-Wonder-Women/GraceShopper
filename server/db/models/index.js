d3241137d65ddb82a327b676e6f5ecb5ae868c58
Siray Wali <siray.yaya@gmail.com>
2021-03-05 14:25:02 -0600

    changed User.js to lowercase

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -20,7 +20,7 @@ Product.belongsToMany(Cart, { through: CartItem });
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
  * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/User')
+ * instead of: const User = require('')
  */
 module.exports = {
 	User,
