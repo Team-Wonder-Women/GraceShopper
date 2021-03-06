e26524167b5df1a6803ded90d98ca7f3831111ba
Sandy Dai <sandydai98@gmail.com>
2021-03-06 17:52:11 -0500

    feat(components/AllProducts): add button to Products and AllProducts component

--- client/components/AllProductsList.js
+++ client/components/AllProductsList.js
@@ -5,14 +5,42 @@ import { getProducts } from "../store/products";
 import Products from "./Products";
 
 class AllProductsList extends Component {
+	constructor(props) {
+		super(props);
+		this.handleAdd = this.handleAdd.bind(this);
+		this.handleSubtract = this.handleSubtract.bind(this);
+		this.state = {
+			count: 1
+		};
+	}
 	componentDidMount() {
 		this.props.loadProducts();
+	}
+
+	handleAdd() {
+		this.setState(prevState => ({ count: prevState.count + 1 }));
+	}
+
+	handleSubtract() {
+		this.state.count < 1
+			? this.setState({
+					count: 0
+			  })
+			: this.setState(prevState => ({ count: prevState.count - 1 }));
 	}
 
 	render() {
 		console.log("all products", this.props);
 		return this.props.products.map(product => {
-			return <Products key={product.id} {...product} />;
+			return (
+				<Products
+					key={product.id}
+					{...product}
+					add={this.handleAdd}
+					subtract={this.handleSubtract}
+					count={this.state.count}
+				/>
+			);
 		});
 	}
 }
