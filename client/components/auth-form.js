c5496e8a4d550f7ad63037b7da393d4f04169ff7
Sandy Dai <sandydai98@gmail.com>
2021-03-08 17:22:29 -0500

    feat(public/style.css): styled signup login form

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -10,38 +10,42 @@ const AuthForm = props => {
 	const { name, displayName, handleSubmit, error } = props;
 	return (
 		<div className="login-signup-container">
-			<h1>{displayName}</h1>
 			<form onSubmit={handleSubmit} name={name}>
+				<h1>{displayName}</h1>
 				{displayName === "Sign Up" && (
-					<div>
-						<label htmlFor="firstName">
-							<small>First Name</small>
-						</label>
-						<input name="firstName" type="text" required />
-						<label htmlFor="lastName">
-							<small>Last Name</small>
-						</label>
-						<input name="lastName" type="text" required />
+					<div className="first-last-name-container">
+						<div>
+							<label htmlFor="firstName">
+								<small>First Name</small>
+							</label>
+							<input name="firstName" type="text" required />
+						</div>
+						<div>
+							<label htmlFor="lastName">
+								<small>Last Name</small>
+							</label>
+							<input name="lastName" type="text" required />
+						</div>
 					</div>
 				)}
 				<div>
 					<label htmlFor="email">
 						<small>Email</small>
 					</label>
-					<input name="email" type="text" required />
+					<input id="email" name="email" type="text" required />
 				</div>
 				<div>
 					<label htmlFor="password">
 						<small>Password</small>
 					</label>
-					<input name="password" type="password" required />
+					<input id="password" name="password" type="password" required />
 				</div>
 				<div>
 					<button type="submit">{displayName}</button>
 				</div>
 				{error && error.response && <div> {error.response.data} </div>}
+				<a href="/auth/google">{displayName} with Google</a>
 			</form>
-			<a href="/auth/google">{displayName} with Google</a>
 		</div>
 	);
 };
