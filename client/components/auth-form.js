e65567ce67ccc678d1164938ccbd4975a9fb8687
Sandy Dai <sandydai98@gmail.com>
2021-03-08 16:06:18 -0500

    feat(components/auth-form): factored in sign up form to form

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -8,21 +8,32 @@ import { auth } from "../store";
  */
 const AuthForm = props => {
 	const { name, displayName, handleSubmit, error } = props;
-	console.log("props -->", props);
 	return (
 		<div>
 			<form onSubmit={handleSubmit} name={name}>
+				{displayName === "Sign Up" && (
+					<div>
+						<label htmlFor="firstName">
+							<small>First Name</small>
+						</label>
+						<input name="firstName" type="text" required />
+						<label htmlFor="lastName">
+							<small>Last Name</small>
+						</label>
+						<input name="lastName" type="text" required />
+					</div>
+				)}
 				<div>
 					<label htmlFor="email">
 						<small>Email</small>
 					</label>
-					<input name="email" type="text" />
+					<input name="email" type="text" required />
 				</div>
 				<div>
 					<label htmlFor="password">
 						<small>Password</small>
 					</label>
-					<input name="password" type="password" />
+					<input name="password" type="password" required />
 				</div>
 				<div>
 					<button type="submit">{displayName}</button>
@@ -64,7 +75,13 @@ const mapDispatch = dispatch => {
 			const formName = evt.target.name;
 			const email = evt.target.email.value;
 			const password = evt.target.password.value;
-			dispatch(auth(email, password, formName));
+			if (formName === "signup") {
+				const firstName = evt.target.firstName.value;
+				const lastName = evt.target.lastName.value;
+				dispatch(auth(email, password, formName, firstName, lastName));
+			} else {
+				dispatch(auth(email, password, formName));
+			}
 		}
 	};
 };
