e3b873d32d00e3f98d9a3e862fb9e687743eafb6
Sandy Dai <sandydai98@gmail.com>
2021-03-09 11:51:31 -0500

    feat(api/userCart): add req.user forbidden condition

--- server/api/userCart.js
+++ server/api/userCart.js
@@ -4,96 +4,112 @@ module.exports = router;
 
 //GET api/userCart/id
 router.get("/:userId", async (req, res, next) => {
-	try {
-		const cart = await Cart.findOrCreate({
-			where: {
-				userId: req.params.userId,
-				orderStatus: "incomplete"
-			}
-		});
-		const cartArr = await cart[0].getProducts();
-		if (cartArr.length) {
-			let totalPrice = 0;
-			cartArr.forEach(item => {
-				totalPrice = totalPrice + item.price * item.cartitem.quantity;
+	if (req.user && req.user.isAdmin) {
+		try {
+			const cart = await Cart.findOrCreate({
+				where: {
+					userId: req.params.userId,
+					orderStatus: "incomplete"
+				}
 			});
-			res.json({ products: cartArr, total: totalPrice });
-		} else {
-			res.status(200).json({ products: null });
+			const cartArr = await cart[0].getProducts();
+			if (cartArr.length) {
+				let totalPrice = 0;
+				cartArr.forEach(item => {
+					totalPrice = totalPrice + item.price * item.cartitem.quantity;
+				});
+				res.json({ products: cartArr, total: totalPrice });
+			} else {
+				res.status(200).json({ products: null });
+			}
+		} catch (err) {
+			next(err);
 		}
-	} catch (err) {
-		next(err);
+	} else {
+		res.sendStatus(403);
 	}
 });
 
 //POST api/userCart/userId/productId --> add-to-cart
 router.post("/:productId", async (req, res, next) => {
-	try {
-		const cart = await Cart.findOrCreate({
-			where: {
-				userId: req.user.id,
-				orderStatus: "incomplete"
+	if (req.user) {
+		try {
+			const cart = await Cart.findOrCreate({
+				where: {
+					userId: req.user.id,
+					orderStatus: "incomplete"
+				}
+			});
+			if (!(await cart[0].hasProduct(req.params.productId))) {
+				const newItem = await Product.findByPk(req.params.productId);
+				await cart[0].addProduct(newItem);
+				CartItem.update(
+					{ priceAtPurchase: newItem.price },
+					{ where: { cartId: cart[0].id, productId: req.params.productId } }
+				);
 			}
-		});
-		if (!(await cart[0].hasProduct(req.params.productId))) {
-			const newItem = await Product.findByPk(req.params.productId);
-			await cart[0].addProduct(newItem);
-			CartItem.update(
-				{ priceAtPurchase: newItem.price },
-				{ where: { cartId: cart[0].id, productId: req.params.productId } }
-			);
+			await CartItem.increment("quantity", {
+				by: req.body.count,
+				where: { cartId: cart[0].id, productId: req.params.productId }
+			});
+			const cartArr = await cart[0].getProducts();
+			let totalPrice = 0;
+			cartArr.forEach(item => {
+				totalPrice = totalPrice + item.price * item.cartitem.quantity;
+			});
+			res.json({ products: cartArr, total: totalPrice });
+		} catch (err) {
+			next(err);
 		}
-		await CartItem.increment("quantity", {
-			by: req.body.count,
-			where: { cartId: cart[0].id, productId: req.params.productId }
-		});
-		const cartArr = await cart[0].getProducts();
-		let totalPrice = 0;
-		cartArr.forEach(item => {
-			totalPrice = totalPrice + item.price * item.cartitem.quantity;
-		});
-		res.json({ products: cartArr, total: totalPrice });
-	} catch (err) {
-		next(err);
+	} else {
+		res.sendStatus(403);
 	}
 });
 
 //GET api/userCart/reduce/productId --> decrement product quantity in cart
 router.put("/:cartId/:productId", async (req, res, next) => {
-	try {
-		const updateItem = await CartItem.findOne({
-			where: { cartId: req.params.cartId, productId: req.params.productId }
-		});
-		if (updateItem.quantity > 1) {
-			await CartItem.decrement("quantity", {
+	if (req.user) {
+		try {
+			const updateItem = await CartItem.findOne({
 				where: { cartId: req.params.cartId, productId: req.params.productId }
 			});
-		} else {
-			await CartItem.destroy({
-				where: { cartId: req.params.cartId, productId: req.params.productId }
+			if (updateItem.quantity > 1) {
+				await CartItem.decrement("quantity", {
+					where: { cartId: req.params.cartId, productId: req.params.productId }
+				});
+			} else {
+				await CartItem.destroy({
+					where: { cartId: req.params.cartId, productId: req.params.productId }
+				});
+			}
+			const cart = await Cart.findByPk(req.params.cartId);
+			const cartArr = await cart.getProducts();
+			let totalPrice = 0;
+			cartArr.forEach(item => {
+				totalPrice = totalPrice + item.price * item.cartitem.quantity;
 			});
-		}
-		const cart = await Cart.findByPk(req.params.cartId);
-		const cartArr = await cart.getProducts();
-		let totalPrice = 0;
-		cartArr.forEach(item => {
-			totalPrice = totalPrice + item.price * item.cartitem.quantity;
-		});
 
-		res.status(202).json({ products: cartArr, total: totalPrice });
-	} catch (err) {
-		next(err);
+			res.status(202).json({ products: cartArr, total: totalPrice });
+		} catch (err) {
+			next(err);
+		}
+	} else {
+		res.sendStatus(403);
 	}
 });
 
 //DELETE api/userCart/cartId/productId ---> delete item from cart
 router.delete("/:cartId/:productId", async (req, res, next) => {
-	try {
-		await CartItem.destroy({
-			where: { cartId: req.params.cartId, productId: req.params.productId }
-		});
-		res.sendStatus(204);
-	} catch (err) {
-		next(err);
+	if (req.user) {
+		try {
+			await CartItem.destroy({
+				where: { cartId: req.params.cartId, productId: req.params.productId }
+			});
+			res.sendStatus(204);
+		} catch (err) {
+			next(err);
+		}
+	} else {
+		res.sendStatus(403);
 	}
 });
